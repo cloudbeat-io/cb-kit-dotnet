7d13d49537f8542792182c8d099fc8c57e8afb5d
N. Dimer <ndimer@cloudbeat.io>
2024-03-13 15:44:50 +0200

    Add CbLink attribute.

---
+++ CloudBeat.Kit.Common/Attributes/CbLink.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CloudBeat.Kit.Common.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = true)]		
+    public class CbLink : Attribute
+    {
+		public CbLink(string name, string url)
+		{
+            Name = name;
+            Url = url;
+		}
+		public string Name { get; internal set; }
+        public string Url { get; internal set; }
+    }
+}
+
