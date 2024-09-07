3e7f7641e13e992bed9841993fbcd5f9e8825112
romovs <romovs@gmail.com>
2024-09-07 10:29:49 +0300

    [common] Mark CB_ENV_AGENT const private & minor code cleanup

--- CloudBeat.Kit.Common/CbTestContext.cs
+++ CloudBeat.Kit.Common/CbTestContext.cs
@@ -4,7 +4,7 @@ namespace CloudBeat.Kit.Common
 {
     public class CbTestContext
 	{
-		public const string CB_ENV_AGENT = "CB_AGENT";
+		private const string CB_ENV_AGENT = "CB_AGENT";
 
 		protected readonly CbTestReporter _reporter;
 		protected readonly CbConfig _config;
@@ -15,28 +15,24 @@ namespace CloudBeat.Kit.Common
 				_reporter = new CbTestReporter(config);
 			_config = config;
 		}
+
 		public CbTestContext(CbConfig config, CbTestReporter reporter)
         {
 			_reporter = reporter;
 			_config = config;
         }
+
 		public static bool IsEnabled
 		{
 			get
 			{
-				var isEnabled = parseBool(Environment.GetEnvironmentVariable(CB_ENV_AGENT));
-				return isEnabled;
+				bool.TryParse(Environment.GetEnvironmentVariable(CB_ENV_AGENT), out bool isEnabled);
+                return isEnabled;
 			}
 		}
+
 		public bool IsConfigured => _reporter != null;
 		public CbTestReporter Reporter => _reporter;
 		public CbConfig Config => _config;
-
-        private static bool parseBool(string str)
-        {
-            if (string.IsNullOrEmpty(str)) return false;
-            bool.TryParse(str, out bool result);
-            return result;
-        }
     }
 }
