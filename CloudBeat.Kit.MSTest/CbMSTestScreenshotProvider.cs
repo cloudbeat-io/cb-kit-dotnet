34c3ee7cabcd41bf4a0fe55d97335db317d47e21
romovs <romovs@gmail.com>
2024-09-07 16:18:59 +0300

    [mstest] Allow taking full page screenshots in chrome

--- CloudBeat.Kit.MSTest/CbMSTestScreenshotProvider.cs
+++ CloudBeat.Kit.MSTest/CbMSTestScreenshotProvider.cs
@@ -1,21 +1,42 @@
 ﻿using CloudBeat.Kit.Common;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.Extensions;
+using System;
 
 namespace CloudBeat.Kit.MSTest
 {
     public class CbMSTestScreenshotProvider : ICbScreenshotProvider
 	{
         private readonly IWebDriver driver;
+        private readonly bool takeFullPageScreenshots;
 
-        public CbMSTestScreenshotProvider(IWebDriver driver)
+        public CbMSTestScreenshotProvider(IWebDriver driver, bool takeFullPageScreenshots)
 		{
             this.driver = driver;
-		}
+            this.takeFullPageScreenshots = takeFullPageScreenshots;
+        }
 
         public string TakeScreenshot()
         {
-            return driver.TakeScreenshot().AsBase64EncodedString;
+            try
+            {
+                if (takeFullPageScreenshots && driver is ChromeDriver)
+                {
+                    try
+                    {
+                        return CbScreenshotHelper.TakeFullPageScreenshot(driver as ChromeDriver);
+                    }
+                    catch
+                    {
+                    }
+                }
+                return driver?.TakeScreenshot()?.AsBase64EncodedString;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
     }
 }
