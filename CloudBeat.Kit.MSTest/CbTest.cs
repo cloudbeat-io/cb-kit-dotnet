a7b9dc2c5d431c0d8b85608152b15c7f448ee3b8
romovs <romovs@gmail.com>
2024-09-09 15:09:51 +0300

    [mstest] Fix hook method, remove instance methods from CbTest and add document methods

--- CloudBeat.Kit.MSTest/CbTest.cs
+++ CloudBeat.Kit.MSTest/CbTest.cs
@@ -1,5 +1,4 @@
-﻿using CloudBeat.Kit.Common.Models;
-using CloudBeat.Kit.MSTest.Attributes;
+﻿using CloudBeat.Kit.MSTest.Attributes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Reflection;
@@ -41,50 +40,6 @@ namespace CloudBeat.Kit.MSTest
             {
                 TestContext?.WriteLine("Error in TestCleanup: " + ex);
             }
-        }
-
-        /// <summary>
-        /// Retrieves a boolean value indicating whether the code is running from CloudBeat.
-        /// </summary>
-        /// <returns><c>true</c> if running from CloudBeat; <c>false</c> otherwise.</returns>
-        public static bool IsRunningFromCB()
-        {
-            return CbMSTest.IsRunningFromCB();
-        }
-
-        /// <summary>
-        /// Adds name/value data pair to the test result.
-        /// </summary>
-        /// <param name="name">Data name.</param>
-        /// <param name="data">Data value.</param>
-        public void AddOutputData(string name, object data)
-        {
-            CbMSTest.AddOutputData(name, data, TestContext);
-        }
-
-        /// <summary>
-        /// Adds name/value test attribute pair to the test result.
-        /// </summary>
-        /// <param name="name">Attribute name</param>
-        /// <param name="value">Attribute value</param>
-        public void AddTestAttribute(string name, object value)
-        {
-            CbMSTest.AddTestAttribute(name, value, TestContext);
-        }
-
-        /// <summary>
-        /// Sets failure reason.
-        /// Could be used from cleanup methods or catch blocks to set reason for the test failure.
-        /// </summary>
-        /// <param name="reason">Failure reason.</param>
-        public void SetFailureReason(FailureReasonEnum reason)
-        {
-            CbMSTest.SetFailureReason(reason, TestContext);
-        }
-
-        public void HasWarnings(bool warnings = true)
-        {
-            CbMSTest.HasWarnings(warnings);
         }
         
         public void Dispose()
