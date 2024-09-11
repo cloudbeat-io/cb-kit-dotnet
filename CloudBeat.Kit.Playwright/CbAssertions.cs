13d4c7c1f24b5a2a9cebd4bd40836e4e013ce263
romovs <romovs@gmail.com>
2024-09-11 15:09:53 +0300

    [playwright] Add CbAssertions

---
+++ CloudBeat.Kit.Playwright/CbAssertions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Playwright;
+
+namespace CloudBeat.Kit.Playwright
+{
+    /// <summary>
+    /// Intended to be used instead of Microsoft.Playwright.Assertions since the later will throw when passed CbLocatorWrapper
+    /// </summary>
+    public static class CbAssertions
+    {
+        public static ILocatorAssertions Expect(ILocator locator) => Assertions.Expect(locator is CbLocatorWrapper wrapper ? wrapper.GetBaseLocator() : locator);
+
+        public static IPageAssertions Expect(IPage page) => Assertions.Expect(page is CbPageWrapper wrapper ? wrapper.GetBasePage() : page);
+    }
+}
